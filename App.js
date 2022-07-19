00b40f776579e5e1e2deeb8a37dd9cf0d003d893
sahanmndl <mondalsahan053@gmail.com>
2022-07-20 00:55:21 +0530

    Initial Commit

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Parse from 'parse/react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
---
+++ src/api/api_keys.js
@@ -0,0 +1,4 @@
+export default {
+    APP_ID: "",
+    JS_KEY: ""
+}
